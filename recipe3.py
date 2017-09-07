9e2a892a71c1146def55d0a9b957f336cfc3837a
Taaniel Magi <28940651+YellowBackCorn@users.noreply.github.com>
2017-09-07 07:08:58 -0700

    Asserting the basics

--- recipe3.py
+++ recipe3.py
@@ -1,14 +1,17 @@
 import unittest
 
+
 class RomanNumericalConverter(object):
     def __init__(self, roman_numerical):
         self.roman_numerical = roman_numerical
-        self.digit_map = {"M":1000, "D":500, "C":100, "L":50, "X":10, "V":5, "I":1}
+        self.digit_map = {"M": 1000, "D": 500, "C": 100, "L": 50, "X": 10, "V": 5, "I": 1}
+
     def convert_to_decimal(self):
         val = 0
         for char in self.roman_numerical:
             val += self.digit_map[char]
         return val
+
 
 class RomanNumericalConverterTest(unittest.TestCase):
     def test_parsing_millenia(self):
@@ -16,8 +19,8 @@ class RomanNumericalConverterTest(unittest.TestCase):
         self.assertEqual(1000, value.convert_to_decimal())
 
     def test_parsing_century(self):
-        value = RomanNumericalConverter("M")
-        self.assertEqual(100, value.convert_to_decimal())
+        value = RomanNumericalConverter("C")
+        self.assertEqual(10, value.convert_to_decimal())
 
     def test_parsing_half_century(self):
         value = RomanNumericalConverter("M")
@@ -43,3 +46,7 @@ class RomanNumericalConverterTest(unittest.TestCase):
     def test_no_roman_numeral(self):
         value = RomanNumericalConverter(None)
         self.assertRaises(TypeError, value.convert_to_decimal)
+
+if __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(RomanNumericalConverter)
+    unittest.TextTestRunner(verbosity=2).run(suite)
