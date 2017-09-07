ce4baf402ad495365e1005d980b3d65b350d67e6
Taaniel Magi <28940651+YellowBackCorn@users.noreply.github.com>
2017-09-07 06:49:08 -0700

    2nd Commit

--- recipe1.py
+++ recipe1.py
@@ -1,46 +1,48 @@
 import unittest
 
 class RomanNumericalConverter(object):
-    def __init__(self, roman_numerical):
-        self.roman_numerical = roman_numerical
+    def __init__(self):
         self.digit_map = {"M":1000, "D":500, "C":100, "L":50, "X":10, "V":5, "I":1}
-    def convert_to_decimal(self):
+    def convert_to_decimal(self, roman_numerical):
         val = 0
-        for char in self.roman_numerical:
+        for char in roman_numerical:
             val += self.digit_map[char]
         return val
 
 class RomanNumericalConverterTest(unittest.TestCase):
+    def setUp(self):
+        print("Creating a new RomanNumericalConverter...")
+        self.cvt = RomanNumericalConverter()
+
+    def tearDown(self):
+        print("Destroying the RomanNumericalConverter...")
+        self.cvt = None
+
     def test_parsing_millenia(self):
-        value = RomanNumericalConverter("M")
-        self.assertEqual(1000, value.convert_to_decimal())
+        self.assertEqual(1000, self.cvt.convert_to_decimal("M"))
 
     def test_parsing_century(self):
-        value = RomanNumericalConverter("M")
-        self.assertEqual(100, value.convert_to_decimal())
+        self.assertEqual(100, self.cvt.convert_to_decimal("C"))
 
     def test_parsing_half_century(self):
-        value = RomanNumericalConverter("M")
-        self.assertEqual(50, value.convert_to_decimal())
+        self.assertEqual(50, self.cvt.convert_to_decimal("L"))
 
     def test_parsing_decade(self):
-        value = RomanNumericalConverter("M")
-        self.assertEqual(10, value.convert_to_decimal())
+        self.assertEqual(10, self.cvt.convert_to_decimal("X"))
 
     def test_parsing_half_decade(self):
-        value = RomanNumericalConverter("M")
-        self.assertEqual(5, value.convert_to_decimal())
+        self.assertEqual(5, self.cvt.convert_to_decimal("V"))
 
     def test_parsing_one(self):
-        value = RomanNumericalConverter("M")
-        self.assertEqual(1, value.convert_to_decimal())
+        self.assertEqual(1, self.cvt.convert_to_decimal("I"))
 
     def test_empty_roman_numeral(self):
-        value = RomanNumericalConverter("")
-        self.assertTrue(value.convert_to_decimal() == 0)
-        self.assertFalse(value.convert_to_decimal() > 0)
+        self.assertTrue(self.cvt.convert_to_decimal("") == 0)
+        self.assertFalse(self.cvt.convert_to_decimal("") > 0)
 
     def test_no_roman_numeral(self):
-        value = RomanNumericalConverter(None)
-        self.assertRaises(TypeError, value.convert_to_decimal)
+        self.assertRaises(TypeError, self.cvt.convert_to_decimal, None)
+
+if __name__ == '__main__':
+    unittest.main()
 
